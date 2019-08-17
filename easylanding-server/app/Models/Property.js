c9d4d19c505849f56148857588c984f70abbc892
Vinicius Cardoso Garcia <vcg@cin.ufpe.br>
2019-08-17 15:53:30 -0300

    Adding Geolocalisation and Controller Setup

--- easylanding-server/app/Models/Property.js
+++ easylanding-server/app/Models/Property.js
@@ -1,9 +1,28 @@
 'use strict'
 
+const Database = use('Database')
+
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Property extends Model {
+  static scopeNearBy(query, latitude, longitude, distance) {
+    /**  cálculo naval de distância através de latitude e longitude conhecido
+     * como Haversine. Esse valor é multiplicado por 6371 que o transforma em
+     * quilômetros.
+     */
+    const haversine = `(6371 * acos(cos(radians(${latitude}))
+    * cos(radians(latitude))
+    * cos(radians(longitude)
+    - radians(${longitude}))
+    + sin(radians(${latitude}))
+    * sin(radians(latitude))))`
+
+    return query
+      .select('*', Database.raw(`${haversine} as distance`))
+      .whereRaw(`${haversine} < ${distance}`)
+  }
+
   user() {
     return this.belongsTo('App/Models/User')
   }
