acb38923770f6ce236ed10a4620615ee04a95a64
Vinicius Cardoso Garcia <vcg@cin.ufpe.br>
2019-08-17 15:20:58 -0300

    Creating CRUD features and relations in REST API
    - Listagem de imóveis;
    - Exibição de um único imóvel com imagens;
    - Remoção de imóveis;
    - Relacionamento entre usuários e imóveis (um para muitos);
    - Relacionamento entre imóveis e imagens (um para muitas);

--- easylanding-server/start/routes.js
+++ easylanding-server/start/routes.js
@@ -16,9 +16,12 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-// Route.get('/', () => {
-//   return { greeting: 'Hello world in JSON' }
-// })
+ Route.get('/', () => {
+   return { greeting: 'Hello world in JSON' }
+ })
 
 Route.post('/users', 'UserController.create')
 Route.post('/sessions', 'SessionController.create')
+Route.resource('properties', 'PropertyController')
+  .apiOnly()
+  .middleware('auth')
